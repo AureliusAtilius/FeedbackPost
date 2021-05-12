a6024ee76f4e7647be222aa8d38c031ccefa2102
Josh <j.j.h.lindner@gmail.com>
2021-05-11 19:54:43 -0700

    Complete script.

--- feedbackPost.py
+++ feedbackPost.py
@@ -3,16 +3,18 @@ import os, requests
 
 # Process directory of text files
 for file in os.listdir("/data/feedback"):
-        with open(file) as file:
+        with open("/data/feedback/"+file) as file:
                 lines = file.readlines()
                 feedback_entry = {}
-                
+
 # Create dictionary with data from each file
                 feedback_entry["title"] = lines[0].strip()
                 feedback_entry["name"] = lines[1].strip()
                 feedback_entry["date"] = lines[2].strip()
                 feedback_entry["feedback"] = " ".join(lines[3:]).strip()
-                #print(feedback_entry)
+                print(feedback_entry)
 
-# Post content to web service api using requests.
-                response = requests.post("https://example.com/path/to/api", data=feedback_entry)
+# Post content to web service api using requests and print the status code.
+                response = requests.post(r"http://example.com/path/to/api", json=feedback_entry)
+                print(response.status_code)
+
